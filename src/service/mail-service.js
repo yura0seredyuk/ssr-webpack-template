618199083c109909073dd08e781769db0ec70396
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-08-22 01:49:01 +0300

    add mail support

--- src/service/mail-service.js
+++ src/service/mail-service.js
@@ -1,6 +1,32 @@
-class MailService {
-    async sendActivationMail(to, link) {
+const nodemailer = require('nodemailer');
 
+class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
+        })
+    }
+
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: 'Activation link' + process.env.API_URL,
+            text: '',
+            html:
+                `
+                    <div>
+                      <h1>Activation link</h1>
+                      <a href="${link}">LINK</a>
+                    </div>
+                `
+        })
     };
 }
 
