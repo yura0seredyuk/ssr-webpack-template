f26f1b15911a33230347cc317920cc2fe970f538
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-09-11 20:32:56 +0300

    fix lint

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import Footer from './ui/client/components/Footer/Footer';
 
 const App = ({ route }) => (
     <div>
-        <Header />
+        <Header/>
         <LanguageToggle />
         <div className={styles.root}>
             {renderRoutes(route.routes)}
         </div>
-        <Footer />
+        <Footer/>
     </div>
 );
 
