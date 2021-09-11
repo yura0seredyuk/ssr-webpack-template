2e2bcc708e686ec336ecff342370a2e0811d6f37
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-09-11 20:55:40 +0300

    fix lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
+import PropTypes from 'prop-types';
 import styles from './App.module.scss';
 import LanguageToggle from './ui/client/components/LanguageToggle/LanguageToggle';
 import Header from './ui/client/components/Header/Header';
 import Footer from './ui/client/components/Footer/Footer';
 
-const App = ({ route }) => (
-    <div>
-        <Header/>
-        <LanguageToggle />
-        <div className={styles.root}>
-            {renderRoutes(route.routes)}
+const App = ({ route }) => {
+    return (
+        <div>
+            <Header/>
+            <LanguageToggle/>
+            <div className={styles.root}>
+                {renderRoutes(route.routes)}
+            </div>
+            <Footer/>
         </div>
-        <Footer/>
-    </div>
-);
+    )
+};
 
 App.defaultProps = {
     route: null
 }
 
 export default { component: App };
+
+App.propTypes = {
+    route: PropTypes.shape({ routes: PropTypes.shape({}) })
+}
